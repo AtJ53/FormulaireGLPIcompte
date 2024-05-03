835b21c6f6ac85758a5dc37bba70f242efd8cb9f
Antoine <127054869+AtJ53@users.noreply.github.com>
2024-05-03 16:09:25 +0200

    Update resetpassword.ps1

--- resetpassword.ps1
+++ resetpassword.ps1
@@ -11,3 +11,6 @@ Set-ADAccountPassword -Identity $Username -Reset -NewPassword (ConvertTo-SecureS
 
 # Activation du compte en cas de besoin (décommenter la ligne suivante si nécessaire)
 Enable-ADAccount -Identity $Username
+
+# Forcer l'utilisateur à changer de mot de passe à la prochaine connexion
+Set-ADUser -Identity $Username -ChangePasswordAtLogon $true
